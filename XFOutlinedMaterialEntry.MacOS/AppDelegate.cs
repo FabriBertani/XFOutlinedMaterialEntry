10d4145265bd1de9da34efec23706ce29a83396b
fbertani <fabricio.bertani@incluit.com>
2021-05-23 06:01:06 -0300

    Added macOS platform.

---
+++ XFOutlinedMaterialEntry.MacOS/AppDelegate.cs
@@ -0,0 +1,42 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace XFOutlinedMaterialEntry.MacOS
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+
+            window.Title = "XFOutlinedMaterialEntry";
+            //window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow
+        {
+            get => window;
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
---
+++ XFOutlinedMaterialEntry.MacOS/Effects/RemoveEntryBordersEffect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AppKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+using XFOutlinedMaterialEntry.MacOS.Effects;
+
+[assembly: ResolutionGroupName("XFOutlinedMaterialEntry")]
+[assembly: ExportEffect(typeof(RemoveEntryBordersEffect), nameof(RemoveEntryBordersEffect))]
+namespace XFOutlinedMaterialEntry.MacOS.Effects
+{
+    public class RemoveEntryBordersEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            var nsTextField = this.Control as NSTextField;
+
+            if (nsTextField is null)
+                throw new NotImplementedException();
+
+            nsTextField.FocusRingType = NSFocusRingType.None;
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
